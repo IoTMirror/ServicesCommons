cbf75953612106848fc5aa967f1b549eb372025a
Tomasz Lewandowski <lewandowski.tomasz93@yahoo.pl>
2016-04-24 19:57:18 +0200

    [Package] Package install dependencies

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name='iotmirror_commons',
       version='0.1',
       description='Common files for IoTMirror Web Services',
       url='https://github.com/IoTMirror/ServicesCommons',
-      packages=['iotmirror_commons'])
+      packages=['iotmirror_commons'],
+      install_requires=['psycopg2']
+     )
