27cb8ca4dbebe8e831b7f52e5c8a7882cb242228
Tomasz Lewandowski <lewandowski.tomasz93@yahoo.pl>
2016-04-24 19:10:34 +0200

    [Package] Python package structure

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='iotmirror_commons',
+      version='0.1',
+      description='Common files for IoTMirror Web Services',
+      url='https://github.com/IoTMirror/ServicesCommons',
+      packages=['iotmirror_commons'])
